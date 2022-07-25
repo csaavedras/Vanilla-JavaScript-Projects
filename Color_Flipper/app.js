213ed4b92163bea2545ef8bcfa89a859d1edadd6
cisaa <ci.saavedras@gmail.com>
2022-07-25 15:40:41 -0400

    Update: new feature random color with rgb()

--- Color_Flipper/app.js
+++ Color_Flipper/app.js
@@ -5,12 +5,15 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', changeBackgroundColor);
 
 function changeBackgroundColor() {
-    const arrayColor = ['blue', 'yellow', 'black'];
-    const randomColor = arrayColor[Math.floor(Math.random() * arrayColor.length)];
+    const arr = [];
 
-    title.innerHTML = `Background Color: ${randomColor}`;
-    background.style.backgroundColor = randomColor;
-    console.log(randomColor);
+    for (let i = 0; i < 3; i++) {
+        const randomNumber = Math.floor(Math.random() * 255);
+        arr.push(randomNumber);
+    }
+    title.innerHTML = `Background Color: RGB (${arr[0]}, ${arr[1]}, ${arr[2]})`;
+    background.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+    btn.style.backgroundColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
 }
 
 
